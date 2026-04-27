1f4917e854120671a0e1fbe6ba95db578a41a802
PythonicDG <dipakgaikwadmg@gmail.com>
2026-04-27 19:22:59 +0530

    seo: update homepage title and description

--- ns2-frontend/src/app/page.js
+++ ns2-frontend/src/app/page.js
@@ -1,7 +1,7 @@
 export const metadata = {
-  title: "MIA | | Home",
+  title: "NS-2 Infotech | Home",
   description:
-    "At MIA, we craft innovative solutions that bridge industry needs and technical training.",
+    "At NS-2 Infotech, we craft innovative solutions that bridge industry needs and technical training.",
 };
 
 export const dynamic = "force-dynamic";
